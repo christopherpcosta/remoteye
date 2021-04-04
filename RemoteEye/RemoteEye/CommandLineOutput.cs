a512461d35482cf3c0b3a24a29b02ef692a9f0b9
Christopher Costa <christopherpereiracosta@gmail.com>
2021-04-05 00:32:29 +0100

    main

---
+++ RemoteEye/RemoteEye/CommandLineOutput.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace RemoteEye
+{
+    public class CommandLineOutput
+    {
+        public List<string> Summary { get; set; }
+    }
+}
---
+++ RemoteEye/RemoteEye/Controllers/Device1Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RemoteEye.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Device1Controller : ControllerBase
+    {
+        private readonly ILogger<Device1Controller> _logger;
+
+        public Device1Controller(ILogger<Device1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                System.IO.File.Copy(@"C:\Output\output.txt", @"C:\Output\outputTemporary.txt", true);
+                var response = System.IO.File.ReadLines(@"C:\Output\outputTemporary.txt");
+
+                var newResponse = new List<string>();
+
+                var originalArray = response.ToArray();
+
+                for (var i = response.Count() - 60; i < response.Count(); i++)
+                {
+                    newResponse.Add(originalArray[i]);
+                }
+
+                return JsonConvert.SerializeObject(newResponse);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+
+        }
+    }
+}
